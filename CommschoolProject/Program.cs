25b2f991e066ae031b860bb6db2297c0b5adec8b
giorgi shushanashvili <giorgishushanashvili@giorgis-MacBook-Air.local>
2023-03-03 16:58:08 +0400

    add new VS solution

---
+++ CommschoolProject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("enter a word");
+var word = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ResetColor();
+Console.WriteLine(word + " is my name");
+
+Console.ReadLine();
+
