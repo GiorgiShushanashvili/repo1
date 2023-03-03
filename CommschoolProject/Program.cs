7509e0fac64b626c0e11c85cbcad07db38c7ed0b
giorgi’s MacBook Air <gioshushanashvili20@icloud.com>
2023-03-03 20:46:10 +0400

    change4

--- CommschoolProject/Program.cs
+++ CommschoolProject/Program.cs
@@ -9,6 +9,9 @@ Console.WriteLine(word + " is my name");
 Console.WriteLine(word is "23 years old");
 Console.WriteLine("i live in tbilisi");
 Console.WriteLine("i play footbal as well");
+Console.WriteLine("hello world");
+Console.WriteLine(123456);
+
 
 Console.ReadLine();
 
