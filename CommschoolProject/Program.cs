04a950fff8612fef4b53aef0c71ea0c5592c7c29
giorgi shushanashvili <giorgishushanashvili@giorgis-MacBook-Air.local>
2023-03-03 17:52:06 +0400

    build and update file

--- CommschoolProject/Program.cs
+++ CommschoolProject/Program.cs
@@ -6,6 +6,9 @@ Console.ForegroundColor = ConsoleColor.Yellow;
 Console.BackgroundColor = ConsoleColor.Red;
 Console.ResetColor();
 Console.WriteLine(word + " is my name");
+Console.WriteLine(word is "23 years old");
+Console.WriteLine("i live in tbilisi");
+Console.WriteLine("i play footbal as well");
 
 Console.ReadLine();
 
